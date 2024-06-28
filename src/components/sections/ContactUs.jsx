8271af60d1d3cc64b9c36211548211432636c152
hazem-alsaqaan <hazem.alsaqaan@gmail.com>
2024-06-28 18:15:41 +0300

    update contact-us scetion container

--- src/components/sections/ContactUs.jsx
+++ src/components/sections/ContactUs.jsx
@@ -7,7 +7,7 @@ const ContactUs = () => {
     const [checkFocus, setCheckFocus] = useState(false)
     return (
         <div id="contact-us" className="py-10 container mx-auto px-4">
-            <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+            <div className="isolate bg-white py-10">
                 <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
                     <div className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"></div>
                 </div>
