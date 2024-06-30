30e6a39aa02de8b2e94151e9a62475cb516129bb
hazem-alsaqaan <hazem.alsaqaan@gmail.com>
2024-06-30 06:19:13 +0300

    fixed toggle navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = ()=>{
     return(
         <div className={`flex-1  justify-end items-center pl-10 max-xsm:flex max-xsm:justify-end `}>
             <FaBars className={`cursor-pointer text-white text-2xl hover:text-yellow-500 duration-200 xsm:hidden`} onClick={()=>setVisibleNav((prev)=>!prev)} />    
-            <ul className={`flex items-center justify-end gap-5  max-sm:gap-3 max-xsm:absolute max-xsm:top-full max-xsm:left-0 max-xsm:bg-blue-500 max-xsm:bg-opacity-85 max-xsm:w-full max-xsm:flex-col max-xsm:items-start max-xsm:p-4 max-xsm:${visibleNav  ? "block" : "hidden"} `}>
+            <ul className={`${visibleNav?"max-xsm:visible":"max-xsm:hidden"} flex items-center justify-end gap-5  max-sm:gap-3 max-xsm:absolute max-xsm:top-full max-xsm:left-0 max-xsm:bg-blue-500 max-xsm:bg-opacity-85 max-xsm:w-full max-xsm:flex-col max-xsm:items-start max-xsm:p-4 `}>
                 <li>
                     <a className={`text-white text-xl max-sm:text-sm hover:text-yellow-400 duration-200`} href="#">Home</a>
                 </li>
